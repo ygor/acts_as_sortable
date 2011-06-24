ea35478a3fd0169ac3f24bb02ddbda10413633e0
Ygor Geurts <ygor.geurts@gmail.com>
2011-06-24 12:01:34 +0200

    corrrectly get table_name from class

--- lib/acts_as_sortable.rb
+++ lib/acts_as_sortable.rb
@@ -15,11 +15,12 @@ module ActsAsSortable #:nodoc:
           if sort[:key] && (sortables.empty? || sortables.include?(sort[:key].to_sym))
             if sort[:key].to_s.include?('.')
               assoc, column = sort[:key].to_s.split('.')
-              reflect_on_association(assoc.to_sym) ? scope.joins(assoc.to_sym).order("`#{assoc.to_s.pluralize}`.`#{column}` #{sort[:dir]}") : scope
+              association_reflection = reflect_on_association(assoc.to_sym)
+              association_reflection ? scope.joins(assoc.to_sym).order("`#{association_reflection.table_name}`.`#{column}` #{sort[:dir]}") : scope
             elsif scope.respond_to?(sort[:key].to_sym) && ![:name, :id].include?(sort[:key].to_sym)
               scope.send(sort[:key].to_sym, sort[:dir].to_s)
             else
-              scope.order("`#{self.name.downcase.pluralize}`.`#{sort[:key]}` #{sort[:dir]}")
+              scope.order("`#{self.table_name}`.`#{sort[:key]}` #{sort[:dir]}")
             end
           else
             scope
