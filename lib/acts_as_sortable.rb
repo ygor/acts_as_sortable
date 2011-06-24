0ef4813c9ac98b768d85d7582bcd571ded8d0fce
Ygor Geurts <ygor.geurts@gmail.com>
2011-06-24 07:44:12 +0200

    cleanup

--- lib/acts_as_sortable.rb
+++ lib/acts_as_sortable.rb
@@ -12,18 +12,14 @@ module ActsAsSortable #:nodoc:
       scope :sorted, lambda {|*sorts|
         sorts = sorts[0] if !sorts.empty? && sorts[0].is_a?(Array)
         sorts.reject(&:nil?).inject(self.scoped) do |scope, sort|
-          unless sort.empty?
-            if sortables.empty? || sortables.include?(sort[:key].to_sym)
-              if sort[:key].to_s.include?('.')
-                assoc, column = sort[:key].to_s.split('.')
-                reflect_on_association(assoc.to_sym) ? scope.joins(assoc.to_sym).order("`#{assoc.to_s.pluralize}`.`#{column}` #{sort[:dir]}") : scope
-              elsif scope.respond_to?(sort[:key].to_sym) && ![:name, :id].include?(sort[:key].to_sym)
-                scope.send(sort[:key].to_sym, sort[:dir].to_s)
-              else
-                scope.order("`#{self.name.downcase.pluralize}`.`#{sort[:key]}` #{sort[:dir]}")
-              end
+          if sort[:key] && (sortables.empty? || sortables.include?(sort[:key].to_sym))
+            if sort[:key].to_s.include?('.')
+              assoc, column = sort[:key].to_s.split('.')
+              reflect_on_association(assoc.to_sym) ? scope.joins(assoc.to_sym).order("`#{assoc.to_s.pluralize}`.`#{column}` #{sort[:dir]}") : scope
+            elsif scope.respond_to?(sort[:key].to_sym) && ![:name, :id].include?(sort[:key].to_sym)
+              scope.send(sort[:key].to_sym, sort[:dir].to_s)
             else
-              scope
+              scope.order("`#{self.name.downcase.pluralize}`.`#{sort[:key]}` #{sort[:dir]}")
             end
           else
             scope
